8441c1148a37ba00adbae2e5aa64d87132bbf288
Denis Kurilenko <Denis Kurilenko>
2016-02-05 14:05:57 +0700

    Init commit, added git ignore files and Visual Studio project for Virgil Mail application

---
+++ outlook/Virgil.Mail/Virgil.Mail/ThisAddIn.cs
@@ -0,0 +1,31 @@
+﻿namespace Virgil.Mail
+{
+    using System;
+
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, EventArgs e)
+        {
+            // Note: Outlook no longer raises this event. If you have code that 
+            // must run when Outlook shuts down, see http://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
