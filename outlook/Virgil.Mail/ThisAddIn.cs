143e5c9805d31808e996f956864598049b392c18
Denis Kurilenko <Denis Kurilenko>
2016-02-05 14:10:14 +0700

    Moved the project to root outlook folder

--- outlook/Virgil.Mail/ThisAddIn.cs
+++ outlook/Virgil.Mail/ThisAddIn.cs
@@ -22,8 +22,8 @@
         /// </summary>
         private void InternalStartup()
         {
-            this.Startup += new System.EventHandler(ThisAddIn_Startup);
-            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+            this.Startup += this.ThisAddIn_Startup;
+            this.Shutdown += this.ThisAddIn_Shutdown;
         }
         
         #endregion
