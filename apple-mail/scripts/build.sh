99cfa7404bc8f4397a5ab2929614d831b4108c99
Roman Kutashenko <kutashenko@gmail.com>
2016-02-22 16:37:29 +0200

    - [APPLE MAIL] test build

--- apple-mail/scripts/build.sh
+++ apple-mail/scripts/build.sh
@@ -58,6 +58,9 @@ function prepare() {
 function build_project() {
 	echo -e "\n------------------ XCode build ----------------------"
 	#xcodebuild -project ${SCRIPT_FOLDER}/../VirgilSecurityMail.xcodeproj -arch x86_64 -configuration Release
+	xcodebuild -workspace ${SCRIPT_FOLDER}/../VirgilSecurityMail.xcworkspace -scheme VirgilFoundation  -arch x86_64 -configuration Release
+	xcodebuild -workspace ${SCRIPT_FOLDER}/../VirgilSecurityMail.xcworkspace -scheme VirgilSDK  -arch x86_64 -configuration Release
+	xcodebuild -workspace ${SCRIPT_FOLDER}/../VirgilSecurityMail.xcworkspace -scheme Pods  -arch x86_64 -configuration Release
 	xcodebuild -workspace ${SCRIPT_FOLDER}/../VirgilSecurityMail.xcworkspace -scheme VirgilSecurityMail  -arch x86_64 -configuration Release
 	check_errors $?
 }
