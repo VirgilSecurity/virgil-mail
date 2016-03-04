0a63bffeff460264bf318b2f371eed5495e0fefb
Roman Kutashenko <kutashenko@gmail.com>
2016-03-04 19:42:43 +0200

    - [APPLE MAIL UPDATER] limit updater work time to 5 minutes

--- apple-mail/VirgilUpdate/AppDelegate.m
+++ apple-mail/VirgilUpdate/AppDelegate.m
@@ -49,6 +49,12 @@
     updater.delegate = self;
     updater.automaticallyDownloadsUpdates = YES;
     [updater checkForUpdatesInBackground];
+    
+    [NSTimer scheduledTimerWithTimeInterval : 300
+                                     target : self
+                                   selector : @selector(terminate)
+                                   userInfo : nil
+                                    repeats : YES];
 }
 
 - (void) printBundleVersion {
