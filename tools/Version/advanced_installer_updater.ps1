b700cc7f139f1d6464d8e7bef339dd43ebbfb12a
Denis Kurilenko <kurilenkodenis@gmail.com>
2016-03-05 18:03:54 +0200

    [MAIL-72] Added build parser

---
+++ tools/Version/advanced_installer_updater.ps1
@@ -0,0 +1,18 @@
+Param (    
+    [Parameter(Mandatory=$true)]
+    [string]$TemplateFilePath
+    [Parameter(Mandatory=$true)]
+    [string]$AssemblyInfoFilePath
+    [Parameter(Mandatory=$true)]
+    [string]$ResultFilePath
+)
+
+$AssemblyInfoContent = [System.IO.File]::ReadAllText($AssemblyInfoFilePath)
+$TemplateContant = [System.IO.File]::ReadAllText($TemplateFilePath)
+
+$versionString = [RegEx]::Match($AssemblyInfoContent,"(AssemblyFileVersion\("")(?:\d+\.\d+\.\d+\.\d+)(""\))")
+
+$ResultContent = [RegEx]::Replace($TemplateContant, "%VERSION%", $versionString)
+
+
+[System.IO.File]::WriteAllText($ResultFilePath, $ResultContent)
