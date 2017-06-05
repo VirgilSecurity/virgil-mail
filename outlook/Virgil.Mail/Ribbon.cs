546d2084bfe737f2665549ce31991c50daab70be
Vasilina Bezuglaya <Vasilina Bezuglaya>
2017-06-05 04:04:41 -0700

    [MAIL-143] fixed error: draft doesn't have button 'encrypt'

---
+++ outlook/Virgil.Mail/Ribbon.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Virgil.Mail.Common;
+using Office = Microsoft.Office.Core;
+
+
+namespace Virgil.Mail
+{
+    [ComVisible(true)]
+    public class Ribbon : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+        public bool isEncryptButtonToggled { get; private set; }
+
+        public Ribbon()
+        {
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            /*
+              string ribbonXML = String.Empty;
+
+            if (ribbonID == "Microsoft.Outlook.Mail.Compose")
+            {
+                ribbonXML = GetResourceText("Trin_RibbonOutlookBasic.Ribbon1.xml");
+            }
+
+            return ribbonXML;
+             */
+            return GetResourceText("Virgil.Mail.Ribbon.xml");
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+            this.isEncryptButtonToggled = Properties.Settings.Default.AutoEncryptEmails;
+        }
+
+        public void encryptButton_Click(Office.IRibbonControl control)
+        {
+            this.isEncryptButtonToggled = ((RibbonToggleButton)control).Checked;
+            Properties.Settings.Default.AutoEncryptEmails = isEncryptButtonToggled;
+            Properties.Settings.Default.Save();
+        }
+        
+        public void mailKeysButton_Click(Office.IRibbonControl control)
+        {
+            ServiceLocator.Dialogs.ShowAccounts();
+        }
+
+        public System.Drawing.Bitmap mailKeysButtonImage(Office.IRibbonControl control)
+        {
+            return global::Virgil.Mail.Properties.Resources.icon_512x512_2x;
+        }
+
+        #endregion
+    }
+}
